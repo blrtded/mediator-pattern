56aece2b073532a925f5873e6c571ffc1b10789f
blrtded <152929869+blrtded@users.noreply.github.com>
2023-12-05 04:29:35 +0100

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,28 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Create Mediator
+        var chatMediator = new ChatMediator();
+
+        // Create Users
+        var user1 = new ChatUser(chatMediator, "User 1");
+        var user2 = new ChatUser(chatMediator, "User 2");
+        var user3 = new ChatUser(chatMediator, "User 3");
+
+        // Add Users to Mediator
+        chatMediator.AddUser(user1);
+        chatMediator.AddUser(user2);
+        chatMediator.AddUser(user3);
+
+        // User 1 sends a message
+        user1.SendMessage("Hello, everyone!");
+
+        // Output:
+        // User 1 sends: Hello, everyone!
+        // User 2 receives: Hello, everyone!
+        // User 3 receives: Hello, everyone!
+    }
+}
